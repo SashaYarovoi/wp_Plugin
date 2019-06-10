81c45997ca1fc0303524567cae8b6a15e0b687b4
weket2434 <45165148+weket2434@users.noreply.github.com>
2019-06-10 12:35:44 +0300

    Update additional_fields.php

--- additional_fields.php
+++ additional_fields.php
@@ -6,11 +6,14 @@ Version: 1.0
 */
 
 function tm_additional_profile_fields( $user ) {
-
-    $floor = array( 'Женский', 'Мужской' );
-    $family_status = array( 'В браке', 'В разводе' , 'Не женат/Не замужем' );
-    $floor_date = wp_parse_args( get_the_author_meta( 'floor_date', $user->ID ), $default );
-    $family_status_date = wp_parse_args( get_the_author_meta( 'family_statusr_date', $user->ID ), $default );
+    
+    $floors = array( 'Женский', 'Мужской' );
+    
+    $family_statuses = array( 'В браке', 'В разводе' , 'Не женат/Не замужем' );
+    
+    $floor_date = get_user_meta( $user->ID, 'floor_date', true);
+    
+    $family_status_date = get_user_meta( $user->ID, 'family_statusr_date', true);
     ?>
     <h3>Дополнительная информация</h3>
 
@@ -20,8 +23,8 @@ function tm_additional_profile_fields( $user ) {
    		 <td>
    			 
    			 <select id="floor-date" name="floor"><?php
-   				 foreach ( $floor as $floor ) {
-   					printf( '<option value="%1$s" %2$s>%1$s</option>', $floor, selected( $floor_date['floor'], $floor, false ) ); 
+   				 foreach ( $floors as $floor ) {
+   					printf( '<option value="%1$s" %2$s>%1$s</option>', $floor, selected( $floor_date, $floor, false ) ); 
    				 }
    			 ?></select>
    			 
@@ -35,8 +38,8 @@ function tm_additional_profile_fields( $user ) {
    		 <td>
    			 
    			 <select id="family-status-date" name="family"><?php
-   				 foreach ( $family_status as $family_status ) {
-   					printf( '<option value="%1$s" %2$s>%1$s</option>', $family_status, selected( $family_status['family'], $family_status, false  ) ); 
+   				 foreach ( $family_statuses as $family_status ) {
+   					printf( '<option value="%1$s" %2$s>%1$s</option>', $family_status, selected( $family_status_date, $family_status, false  ) ); 
    				 }
    			 ?></select>
    			 
@@ -62,4 +65,6 @@ function modify_contact_methods( $contact_fields ) {
 add_filter('user_contactmethods', 'modify_contact_methods');
 
 add_user_meta( $user_id, $meta_key, $meta_value, $unique );
+
+
 
